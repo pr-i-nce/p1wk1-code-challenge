d52ee055c0403a8db941b6c6839068e790b9a539
prince mumo <princeinkunzi@gmail.com>
2024-04-01 14:12:24 +0300

    first commit

--- grade.js
+++ grade.js
@@ -7,13 +7,9 @@ function gradeMarks() {
   
   // Get the marks value from the input element
   let marks = parseFloat(marksInput.value);
-  
   // To check if the entry is  a number
-  if (isNaN(marks) || marks < 0 || marks > 100) {
-      // If the input is not a number within the range of 0 to 100, display an error message
-      grade.textContent = 'Invalid entry. Marks should be between 0 and 100.';
-  } else {
-      // Determine the grade according to the input marks
+  
+  
       if (marks > 79) {
           grade.textContent = 'Excellent!!! You managed to score an A !!!';
       } else if (marks >= 60) {
@@ -22,9 +18,11 @@ function gradeMarks() {
           grade.textContent = 'Almost there! Keep working.You managed to score a C';
       } else if (marks >= 40) {
           grade.textContent = 'Aim higher.You scored a D';
-      } else {
+      } else if (marks <40){
           grade.textContent = 'Aim higher.You scored an E';
       }
+        else () =>{
+            grade,textContent = 'Invalid entry';
+        }
   }
-}
 
